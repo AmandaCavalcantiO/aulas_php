a1b706c631fab2ff6ae443a4e2dddf37eecb5341
Amanda <amanda.cavalcanti06@gmail.com>
2023-02-23 21:22:00 -0300

    filtros nos formularios

--- 03-manipulacao-e-tratamento/03-09-formularios-e-filtros/index.php
+++ 03-manipulacao-e-tratamento/03-09-formularios-e-filtros/index.php
@@ -1,4 +1,5 @@
 <?php
+
 require __DIR__ . '/../../fullstackphp/fsphp.php';
 fullStackPHPClassName("03.09 - Formuários e filtros");
 
@@ -7,14 +8,34 @@ fullStackPHPClassName("03.09 - Formuários e filtros");
  * https://php.net/manual/pt_BR/book.filter.php
  */
 fullStackPHPClassSession("request", __LINE__);
+$form = new stdClass();
+$form->name = 'Amanda C';
+$form->mail = 'amanda.c@mail.com';
 
+var_dump($_REQUEST);
+$form->method = 'post';
+include __DIR__ . '/form.php';
 
 /*
  * [ post ] $_POST | INPUT_POST | filter_input | filter_var
  */
 fullStackPHPClassSession("post", __LINE__);
-
-
+include __DIR__ . '/form.php';
+$post['name'] = filter_input(INPUT_POST, 'name', FILTER_DEFAULT);
+$postArray = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+var_dump($post, $postArray);
+if ($postArray) {
+    if (in_array('', $postArray)) {
+        echo "<p class= 'trigger warning'> Preencha todos os campos!</p>";
+    } elseif (!filter_var($postArray['mail'], FILTER_VALIDATE_EMAIL)) {
+        echo "<p class= 'trigger warning'> E-mail inválido!</p>";
+    } else {
+        $postArray = array_map('strip_tags', $postArray);
+        $save = array_map('trim', $postArray);
+        var_dump($save);
+        echo "<p class= 'trigger accept'>Cadastro realizado com sucesso!</p>";
+    }
+}
 /*
  * [ get ] $_GET | INPUT_GET | filter_input | filter_var
  */
@@ -26,3 +47,18 @@ fullStackPHPClassSession("get", __LINE__);
  * http://php.net/manual/pt_BR/filter.constants.php
  */
 fullStackPHPClassSession("filters", __LINE__);
+var_dump(
+    filter_list(),
+    [filter_id('validate_email'),
+    FILTER_VALIDATE_DOMAIN,
+    FILTER_SANITIZE_STRING]
+);
+$filterForm = [
+    'name' => FILTER_SANITIZE_STRIPPED,
+    'mail' => FILTER_VALIDATE_EMAIL
+];
+$getForm = filter_input_array(INPUT_POST, $postArray);
+var_dump($getForm);
+$email = 'aaaa.com';
+var_dump([filter_var($email, FILTER_VALIDATE_EMAIL),
+    filter_var_array($getForm, $filterForm)]);
